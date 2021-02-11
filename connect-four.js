62fa19459cae6e0b6de2b28ecab748e141dda014
Andrew Moss <andrewmoss@Andrews-MacBook-Pro.local>
2021-02-11 17:18:21 -0600

    indicating current player

--- connect-four.js
+++ connect-four.js
@@ -1,35 +1,38 @@
-import {Game} from './game.js'
+import { Game } from './game.js'
 
+const boardHolder = document.querySelector('#board-holder')
 let game = undefined
+const gameName = document.querySelector('#game-name')
+
+const updateUI = function () {
+    if (game === undefined) {
+        boardHolder.classList.add('is-invisible')
+    }
+    else {
+        boardHolder.classList.remove('is-invisible')
+        gameName.innerHTML = game.getName()
+    }
+}
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    const colSelectors = document.querySelectorAll('.click-target')
+    const clickTargets = document.querySelector('#click-targets')
+    console.log('clickTargets:', clickTargets)
     const boardSquares = document.querySelectorAll('.token-square')
-
-// Column organized into nested array
-    // let ColofSquares = []
-    // for (let i=0;i<colSelectors.length;i++) {
-    //     let col = colSelectors[i] // col 0
-    //     let array = []
-    //     for(let j=0;j<boardSquares.length;j++) {
-    //         let square = boardSquares[j].id.split('-')[1]
-    //         if (square === col.id.split('-')[1]) {
-    //             array.push(boardSquares[j])
-    //         }
-    //     }
-    //     if (array.length !== 0) {
-    //     ColofSquares.push(array)
-    //     }
-    // }
-
     const player1 = document.getElementById('player-1-name')
     const player2 = document.getElementById('player-2-name')
-    const players = [player1,player2]
+    const players = [player1, player2]
     const newgame = document.getElementById('new-game')
 
+
+    clickTargets.addEventListener('click', event => {
+        game.playInColumn()
+        updateUI()
+    });
+
+
     player1.addEventListener('keyup', e => {
-        if(!player1.value && !player2.value ) {
+        if (!player1.value && !player2.value) {
             newgame.disabled = true
         }
 
@@ -39,7 +42,7 @@ window.addEventListener('DOMContentLoaded', event => {
     })
 
     player2.addEventListener('keyup', e => {
-        if(!player1.value && !player2.value ) {
+        if (!player1.value && !player2.value) {
             newgame.disabled = true
         }
 
@@ -47,6 +50,23 @@ window.addEventListener('DOMContentLoaded', event => {
             newgame.disabled = false
         }
     })
+
+    newgame.addEventListener('click', event => {
+        game = new Game(player1name, player2name)
+
+        this.player1name = '';
+        this.player2name = '';
+
+
+        newgame.disabled = true
+
+        updateUI();
+
+
+
+    })
+
+
 
 
 
--- game.js
+++ game.js
@@ -1,13 +1,43 @@
 class Game {
-    constructor(player1name,player2name) {
+    constructor(player1name, player2name) {
         this.player1name = player1name
         this.player2name = player2name
+        this.currentPlayer = 1
     }
 
     getName() {
         return `${this.player1name} versus ${this.player2name}`
     }
+
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2
+        } else {
+            this.currentPlayer = 1;
+        }
+    }
 }
 
 
-export {Game}
+export { Game }
+
+
+
+
+
+
+            // Column organized into nested array
+            // let ColofSquares = []
+            // for (let i=0;i<colSelectors.length;i++) {
+            //     let col = colSelectors[i] // col 0
+            //     let array = []
+            //     for(let j=0;j<boardSquares.length;j++) {
+            //         let square = boardSquares[j].id.split('-')[1]
+            //         if (square === col.id.split('-')[1]) {
+            //             array.push(boardSquares[j])
+            //         }
+            //     }
+            //     if (array.length !== 0) {
+            //     ColofSquares.push(array)
+            //     }
+            // }
